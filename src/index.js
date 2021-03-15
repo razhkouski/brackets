59acdd4f6615d47b8f79436ab67c190af5879c1a
razhkouski <i.razhkouski@gmail.com>
2021-03-15 20:28:37 +0300

    feat: implement brackets checker function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let bracket of str) {
+      for (let subArray of bracketsConfig) {
+          if (bracket === subArray[0]) {
+              if (subArray[0] === subArray[1] && stack[stack.length - 1] === subArray[0]) {
+                  stack.pop();
+              } else {
+                  stack.push(bracket);
+              }
+          } else {
+              if (bracket === subArray[1] && stack[stack.length - 1] !== subArray[0]) {
+                  return false;
+              } else if (bracket === subArray[1] && stack[stack.length - 1] === subArray[0]) {
+                  stack.pop();
+              }
+          }
+      }
+  }
+  return (stack.length === 0);
 }
